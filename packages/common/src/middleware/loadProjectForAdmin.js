7ab85b96f0c566bc9b7ffab8f4e426eb11646db4
Nitin Kumar Yadav <nitinyadav8675@gmail.com>
2026-07-03 16:20:51 +0530

    fixed in loadprojectforadmin

--- packages/common/src/middleware/loadProjectForAdmin.js
+++ packages/common/src/middleware/loadProjectForAdmin.js
@@ -1,13 +1,17 @@
 // FUNCTION - LOAD PROJECT FOR ADMIN (MIDDLEWARE)
 const Project = require('../models/Project');
 const AppError = require('../utils/AppError');
+const { getProjectAccessQuery } = require('../utils/projectAccess');
 
 module.exports = async (req, res, next) => {
     try {
         const { projectId } = req.params;
         if (!projectId) return next(new AppError(400, "Project ID is required"));
 
-        const project = await Project.findOne({ _id: projectId, owner: req.user._id });
+        const project = await Project.findOne({
+            _id: projectId,
+            ...getProjectAccessQuery(req.user._id),
+        });
         if (!project) {
             return next(new AppError(404, "Project not found or access denied"));
         }
